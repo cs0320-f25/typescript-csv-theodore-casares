92e5676ddec59cd4d65a27508d940654b09f07a1
theodore-casares <theodore_casares@brown.edu>
2025-09-12 23:44:12 -0400

    getting rid of diff markers, apparently had a merge conflict, this is taking forever

--- src/run-parser.ts
+++ src/run-parser.ts
@@ -4,11 +4,7 @@ import { parseCSV } from "./basic-parser";
   Example of how to run the parser outside of a test suite.
 */
 
-<<<<<<< HEAD
 const DATA_FILE = "./data/sayings.csv"; // update with your actual file name
-=======
-const DATA_FILE = "./data/people.csv"; // update with your actual file name
->>>>>>> fbfd3f0e3c288d9d0a5d814877ec2e7ac83f99bd
 
 async function main() {
   // Because the parseCSV function needs to "await" data, we need to do the same here.
@@ -18,11 +14,7 @@ async function main() {
   // another iterates over the indexes only.
   for(const record of results)
     console.log(record)
-<<<<<<< HEAD
   for(const record in results) //in iterates over indexes (012345)
-=======
-  for(const record in results)
->>>>>>> fbfd3f0e3c288d9d0a5d814877ec2e7ac83f99bd
     console.log(record)
 }
 
